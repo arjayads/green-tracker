85e021bfc4bb78836e5147472f40cf06899c8cba
Arjay Ads <arjay@verticalops.com>
2015-09-22 05:34:47 +0800

    remove old files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,66 +5,59 @@
 <head>
     <title>Login</title>
     <link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="/css/app/clinic.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="middle-box loginscreen">
-    <h3 class="text-primary text-center">Green Trackerr</h3>
+<div style="text-align: center">
+    <div class="col-md-offset-4 col-md-3">
+        <h3 class="text-primary text-center">Green Tracker</h3>
 
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul style="list-style-type: none; padding-left: 5px;">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form method="POST" action="/auth/login">
-        <fieldset>
-            {!! csrf_field() !!}
-
-            <div class="row">
-                <div class="col-md-3">
-                    <label for="email">Email</label>
-                </div>
-                <div class="col-md-9">
-                    <input class="form-control" type="email" id="email" name="email" value="{{ old('email') }}">
-                </div>
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul style="list-style-type: none; padding-left: 5px;">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
+        @endif
 
-            <div class="hoz-space"></div>
-            <div class="row">
-                <div class="col-md-3">
-                    <label for="password">Password</label>
-                </div>
-                <div class="col-md-9">
-                    <input type="password" class="form-control" id="password" name="password">
-                </div>
-            </div>
+        <form method="POST" action="/auth/login">
+            <fieldset>
+                {!! csrf_field() !!}
 
-            <div class="hoz-space"></div>
-            <div class="row">
-                <div class="col-md-3">
+                <div class="row">
+                    <div class="col-md-12">
+                        <input placeholder="Email" class="form-control" type="email" id="email" name="email" value="{{ old('email') }}">
+                    </div>
                 </div>
-                <div class="col-md-9">
-                    <label><input type="checkbox" name="remember"> Remember Me</label>
-                </div>
-            </div>
 
-            <div class="hoz-space"></div>
-            <div class="row">
-                <div class="col-md-12">
-                    <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+                <div class="hoz-space"></div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <input placeholder="Password" type="password" class="form-control" id="password" name="password">
+                    </div>
                 </div>
-            </div>
-        </fieldset>
-    </form>
+
+                <div class="hoz-space"></div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <label><input type="checkbox" name="remember"> Remember Me</label>
+                    </div>
+                </div>
+
+                <div class="hoz-space"></div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </div>
 </div>
 
-<script type="text/javascript" src="/js/jquery/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/vendor/jquery.min.js"></script>
+<script type="text/javascript" src="/js/vendor/bootstrap.min.js"></script>
 
 </body>
 </html>

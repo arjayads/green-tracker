281bf316d0709b751ebfdbbc7e401d9ac9f8cf2c
Arjay Ads <arjay@verticalops.com>
2015-10-06 23:58:31 +0800

    add files table

--- database/migrations/2014_10_11_140735_create_files_table.php
+++ database/migrations/2014_10_11_140735_create_files_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,20 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table)
+        {
             $table->engine = 'InnoDB';
 
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->smallInteger('active');
-            $table->rememberToken();
-            $table->unsignedInteger('profile_photo_file_id')->nullable();
+            $table->string('new_filename', 65535);
+            $table->string('orig_filename', 65535);
+            $table->string('mime_type');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-
-            $table->foreign('profile_photo_file_id')->references('id')->on('files')->onDelete('set null');
-
         });
     }
 
@@ -36,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('files');
     }
 }

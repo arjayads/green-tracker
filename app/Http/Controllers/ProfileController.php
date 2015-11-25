f5455cbd396f5bafad3c298240c11ab2366637da
Arjay Ads <arjay@verticalops.com>
2015-11-26 01:58:06 +0800

    update validation import

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,7 +10,7 @@ use app\Services\ProfileService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
-use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {

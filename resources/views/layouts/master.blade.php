9662058d81fa12574e08dda42a5e468d240ac7dc
Arjay Adong <arjayads@gmail.com>
2015-12-29 01:08:51 +0800

    update includes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,8 +44,7 @@
 @include('includes.nav')
 @yield('content')
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="/js/vendor/jquery.min.js"></script>
 <script src="/js/vendor/modernizr.min.js"></script>
 <script src="/js/vendor/pace.min.js"></script>
 <script src="/js/vendor/bootstrap.min.js"></script>

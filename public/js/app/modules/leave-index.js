069ce978e1b28ad48d49d45f334653412dae735a
Arjay Adong <arjayads@gmail.com>
2015-12-30 00:42:58 +0800

    leave list - set default status filter

--- public/js/app/modules/leave-index.js
+++ public/js/app/modules/leave-index.js
@@ -1,17 +1,17 @@
 var leaveIndexApp = angular.module('leaveIndex', ['config', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.resizeColumns']);
 
 leaveIndexApp.controller('mainCtrl', ['$scope', '$http', '$filter', '$sce', function ($scope, $http, $filter, $sce) {
-    $scope.title = 'Your leave is like a sun!';
-    $scope.status = 'Pending';
+    $scope.title = 'Leave Monitoring';
+    $scope.selectedStatus = {id:1, status: 'Pending'};
     $scope.leaves = [];
     $scope.stats = [
-        {id:1, status: 'Pending'},
+        $scope.selectedStatus,
         {id:2, status: 'Approved'},
         {id:3, status: 'Disapproved'}
     ];
 
     var getLeaves = function() {
-        $http.get('/my/leaves/' + $scope.status).success(function(data){
+        $http.get('/my/leaves/' + $scope.selectedStatus.status).success(function(data){
             $scope.leaves = data;
             $scope.gridOptions1.data = data;
         });
@@ -25,6 +25,7 @@ leaveIndexApp.controller('mainCtrl', ['$scope', '$http', '$filter', '$sce', func
                 var date = ds[i];
                 str += '<span class="label label-info">' + $filter('date')(date, 'MMM d, yyyy') + '</span> ';
             }
+
             return $sce.trustAsHtml(str);
         }
     }
@@ -39,58 +40,27 @@ leaveIndexApp.controller('mainCtrl', ['$scope', '$http', '$filter', '$sce', func
         paginationPageSize: 15,
         useExternalPagination: true,
         enableSorting: true,
-        //columnDefs: [
-        //    {
-        //        field: 'order_number',
-        //        displayName: 'Order No',
-        //        width: '9%',
-        //        enableHiding: false,
-        //        cellTemplate: '<div class="ui-grid-cell-contents"><a href="{{grid.appScope.buildCellUrl(row.entity.id)}}">{{row.entity.order_number}}</a></div>'
-        //
-        //    },
-        //    {field: 'campaign_name', displayName: 'Campaign', enableHiding: false},
-        //    {field: 'product_name', displayName: 'Product', enableHiding: false},
-        //    {
-        //        field: 'date_sold',
-        //        type: 'date',
-        //        cellFilter: 'date:\'MMM dd, yyyy\'',
-        //        enableHiding: false
-        //    },
-        //    {
-        //        field: 'customer.first_name',
-        //        displayName: 'Customer',
-        //        cellTemplate: '<div class="ui-grid-cell-contents">{{row.entity.customer.first_name + " " + row.entity.customer.last_name}}</div>',
-        //        enableHiding: false
-        //
-        //    },
-        //    {field: 'customer.phone_number', displayName: 'Phone No', enableHiding: false},
-        //    {field: 'processed_by', displayName: 'Agent' , enableHiding: false},
-        //    {
-        //        field: 'verified',
-        //        displayName: '',
-        //        enableSorting: false,
-        //        enableHiding: false,
-        //        width: '2%',
-        //        cellTemplate: '<div id="sav" class="ui-grid-cell-contents"><i ng-click="grid.appScope.setVerified(row.entity)" title="Set as verified" class="cursor fa-2x fa {{row.entity.verified == \'0\' ? \'fa-check-circle \':\'\'}} "></i></div>'
-        //    },
-        //],
+        columnDefs: [
+            {
+                field: 'date_filed',
+                type: 'date',
+                cellFilter: 'date:\'MMM dd, yyyy\'',
+                enableHiding: false
+            },
+            {field: 'leave_type', displayName: 'Leave Type', enableHiding: false},
+            {field: 'purpose', displayName: 'Purpose', enableHiding: false},
+            {field: 'no_of_days', displayName: 'No of days', enableHiding: false},
+
+            {
+                field: 'dates',
+                displayName: 'Inclusive dates',
+                enableHiding: false,
+                cellTemplate: '<div class="ui-grid-cell-contents" ng-bind-html="grid.appScope.parseDates(row.entity.dates)"></div>'
+
+            },
+        ],
         onRegisterApi: function(gridApi) {
             $scope.gridApi = gridApi;
-            //$scope.gridApi.core.on.sortChanged($scope, function(grid, sortColumns) {
-            //    if (sortColumns.length == 0) {
-            //        paginationOptions.sort = 'asc';
-            //        paginationOptions.sortCol = 'order_number';
-            //    } else {
-            //        paginationOptions.sort = sortColumns[0].sort.direction;
-            //        paginationOptions.sortCol = sortColumns[0].field;
-            //    }
-            //    getSaleList();
-            //});
-            //gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-            //    paginationOptions.pageNumber = newPage;
-            //    paginationOptions.pageSize = pageSize;
-            //    getSaleList();
-            //});
         }
     }
 
--- resources/views/employee/index-leave.blade.php
+++ resources/views/employee/index-leave.blade.php
@@ -21,7 +21,6 @@
                                 <div class="col-md-2 col-lg-2 pull-right">
                                     <select style="padding: 0" required="" ng-change="setSelectedStatus(selectedStatus)" class="form-control" id="status" ng-model="selectedStatus"
                                             ng-options="st.status for st in stats track by st.id">
-                                        <option value="">Select Status</option>
                                     </select>
                                 </div>
                             </div>
@@ -32,26 +31,6 @@
                                 <div ui-grid="gridOptions1" ui-grid-pagination ui-grid-resize-columns id="list"></div>
                             </div>
                         </div>
-                        <table class="table table-bordered table-responsive">
-                            <thead>
-                            <tr>
-                                <th>Date Filed</th>
-                                <th>Leave Type</th>
-                                <th>Purpose</th>
-                                <th>No of days</th>
-                                <th>Inclusive dates</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr ng-repeat="leave in leaves track by $index">
-                                <td><%leave.date_filed | date: 'MMM d, yyyy'%></td>
-                                <td><%leave.leave_type%></td>
-                                <td><%leave.purpose%></td>
-                                <td><%leave.no_of_days%></td>
-                                <td ng-bind-html="parseDates(leave.dates)"></td>
-                            </tr>
-                            </tbody>
-                        </table>
                     </div>
                 </main>
             </div>
@@ -63,4 +42,9 @@
 @section('javascript')
     <script src="{{asset('js/app/modules/leave-index.js')}}"></script>
     <script src="{{asset('js/ui-grid/ui-grid.min.js')}}"></script>
+    <script>
+        $( document ).ready(function() {
+            $('.ui-grid-icon-angle-down').remove();
+        });
+    </script>
 @stop
